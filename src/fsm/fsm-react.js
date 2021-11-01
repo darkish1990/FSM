c744c9fe7e84459da82e9ba3e362fc65adf5ebd7
roey liba <v-roeyliba@microsoft.com>
2021-11-01 12:10:39 +0200

    remove hook dependency on new fsm state update

--- src/fsm/fsm-react.js
+++ src/fsm/fsm-react.js
@@ -18,12 +18,6 @@ export const useFSM = (fsm) => {
     };
   }, [fsm, onTransition, onError]);
 
-  useEffect(() => {
-    if (fsm.getState() !== state) {
-      setState(fsm.getState());
-    }
-  }, [fsm, state]);
-
   const transition = useCallback(
     (transitionName) => {
       fsm.transition(transitionName);
